d3b9fa28e3143f082d5482e3313565e542bdd8f2
Patrice Allardin <patrice.allardin@free.fr>
2019-04-16 16:09:39 +0200

    3ème commit

--- src/main/java/fr/tableaux/Exercice2.java
+++ src/main/java/fr/tableaux/Exercice2.java
@@ -4,6 +4,9 @@ public class Exercice2 {
 
 	public static void main(String[] args) {
 		int [][] tab = {{1,2,3,5,6,7,8,9,10}, {4,5,6,7,8,9}, {1,2}};
+		int [][] tab2 = new int [2][];
+		tab2[0] = new int[] {1,2,3,4,5};
+		tab2[1] = new int[] {6,7,8,9,10};
 		System.out.println("Longueur de tableau : " + tab.length);
 		System.out.println("Longueur de tableau 0 : " + tab[0].length);
 		System.out.println("Longueur de tableau 1 : " + tab[1].length);
