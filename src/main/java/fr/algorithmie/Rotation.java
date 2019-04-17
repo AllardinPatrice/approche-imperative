5c59ed5cb1a08d4a5e6301d0fb0a8dc36b24e6ad
Patrice Allardin <patrice.allardin@free.fr>
2019-04-17 12:30:33 +0200

    7ème commit

---
+++ src/main/java/fr/algorithmie/Rotation.java
@@ -0,0 +1,18 @@
+package fr.algorithmie;
+
+public class Rotation {
+
+	public static void main(String[] args) {
+		int[] array2 = {6, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 8} ;
+		int c = array2[array2.length-1];
+		for (int i = array2.length-1; i > 0  ; i--) {
+			array2[i] = array2[i-1];
+		}
+		array2[0] = c;
+
+		for (int i = 0; i < array2.length ; i++) {
+			System.out.println("Rotation : indice : " + i + " valeur : " + array2[i]);
+		}
+	}
+
+}
--- src/main/java/fr/algorithmie/TriParSelection.java
+++ src/main/java/fr/algorithmie/TriParSelection.java
@@ -4,13 +4,26 @@ public class TriParSelection {
 
 	public static void main(String[] args) {
 		int[] array2 = {3, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 8} ;
-		for (int i = 0; i < i < array2.length -2){
-			
-		
+		for (int i = 0; i < array2.length -1; i++){
 			int min = i;
-				for (int j = i+1; array2.length -1)
+			for (int j = i+1; j < array2.length ; j++) {
+				if (array2[j] < array2[min] ) {
+					min = j;
+				}
+			}
+			if (min != i) {
+				int c;
+				c = array2 [i];
+				array2[i] = array2[min];
+				array2[min] = c;
+				
+			}
 			
 		}
+		for (int i = 0 ; i < array2.length ; i++) {
+			System.out.println("Le tableau trié est : indice : " + i + " valeur : " + array2[i]);	
+		}
+		
 	}
 
 }
