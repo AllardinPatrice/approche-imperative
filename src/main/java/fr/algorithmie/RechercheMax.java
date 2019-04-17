5b18fc01971ff80739af04d4a240fc8419f191db
Patrice Allardin <patrice.allardin@free.fr>
2019-04-17 10:11:29 +0200

    5ème commit

--- src/main/java/fr/algorithmie/RechercheMax.java
+++ src/main/java/fr/algorithmie/RechercheMax.java
@@ -4,7 +4,7 @@ public class RechercheMax {
 
 	public static void main(String[] args) {
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
-		int plusGrand = -1000;
+		int plusGrand = array[0];
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] > plusGrand) {
 				plusGrand = array [i];
--- src/main/java/fr/algorithmie/RechercheMin.java
+++ src/main/java/fr/algorithmie/RechercheMin.java
@@ -4,7 +4,7 @@ public class RechercheMin {
 
 	public static void main(String[] args) {
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
-		int plusPetit = 1000;
+		int plusPetit = array[0];
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] < plusPetit) {
 				plusPetit = array [i];
